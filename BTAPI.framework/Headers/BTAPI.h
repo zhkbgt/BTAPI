dad6d2a19f254d4d6339600553c9a85fb8228ab8
zh神夕 <kkbbggtt@163.com>
2019-03-28 23:47:50 +0800

    0.1.0

---
+++ BTAPI.framework/Headers/BTAPI.h
@@ -0,0 +1,34 @@
+//
+//  BTAPI.h
+//  BTAPI
+//
+//  Created by 张兵 on 2019/3/28.
+//  Copyright © 2019 张兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BTAPI.
+FOUNDATION_EXPORT double BTAPIVersionNumber;
+
+//! Project version string for BTAPI.
+FOUNDATION_EXPORT const unsigned char BTAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BTAPI/PublicHeader.h>
+
+#import <BTAPI/APIWindowContainer.h>
+#import <BTAPI/APIWidgetContainer.h>
+#import <BTAPI/APIManager.h>
+#import <BTAPI/APIEvent.h>
+#import <BTAPI/APIUpdateManager.h>
+#import <BTAPI/APIWebView.h>
+#import <BTAPI/APIScriptMessage.h>
+#import <BTAPI/APIModuleMethod.h>
+
+#import <BTAPI/UZAppDelegate.h>
+#import <BTAPI/UZWidget.h>
+#import <BTAPI/UZAppUtils.h>
+#import <BTAPI/UZModule.h>
+#import <BTAPI/APIWebView.h>
+#import <BTAPI/NSDictionaryUtils.h>
+#import <BTAPI/UZModule.h>
